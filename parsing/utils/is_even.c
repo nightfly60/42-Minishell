27c0dbea0c5cf9ababac1ab0136231521cae7e17
Elena Duranceau <edurance@f6r5s7.clusters.42paris.fr>
2025-08-15 11:24:36 +0200

    merge

--- parsing/utils/is_even.c
+++ parsing/utils/is_even.c
@@ -6,20 +6,13 @@
 /*   By: edurance <edurance@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 13:24:24 by edurance          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/08/15 11:02:09 by edurance         ###   ########.fr       */
-=======
-/*   Updated: 2025/08/14 13:24:47 by edurance         ###   ########.fr       */
->>>>>>> anas
+/*   Updated: 2025/08/15 11:24:27 by edurance         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../parsing.h"
 
-<<<<<<< HEAD
 /*Est ce que ce nombre est pair ou impair. even = pair / odd = impair.*/
-=======
->>>>>>> anas
 int	is_even(int n)
 {
 	if (n % 2 == 0)
