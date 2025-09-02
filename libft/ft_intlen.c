d8d6849e7d8d7c8287355feb92ba5e8844a1d3fa
Elena Duranceau <edurance@f1br6s12.clusters.42paris.fr>
2025-09-02 12:19:05 +0200

    libft/ft_intlen.c

--- libft/ft_intlen.c
+++ libft/ft_intlen.c
@@ -1,42 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*   ft_intlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edurance <edurance@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/26 11:48:40 by edurance          #+#    #+#             */
-/*   Updated: 2025/09/02 12:18:02 by edurance         ###   ########.fr       */
+/*   Created: 2025/09/02 12:17:33 by edurance          #+#    #+#             */
+/*   Updated: 2025/09/02 12:17:54 by edurance         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
+int	ft_intlen(int n)
 {
-	char	*res;
 	long	nb;
-	int		nlen;
+	int		count;
 
-	nlen = ft_intlen(n);
 	nb = (long)n;
-	res = malloc(sizeof(char) * nlen + 1);
-	if (!res)
-		return (NULL);
-	if (nb == 0)
-		res[0] = '0';
-	if (nb < 0)
+	count = 0;
+	if (nb <= 0)
 	{
-		res[0] = '-';
+		count++;
 		nb = -nb;
 	}
-	res[nlen] = '\0';
-	nlen--;
 	while (nb > 0)
 	{
-		res[nlen] = nb % 10 + '0';
 		nb = nb / 10;
-		nlen--;
+		count++;
 	}
-	return (res);
+	return (count);
 }
