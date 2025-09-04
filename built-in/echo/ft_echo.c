b906312f95aac54ab01614de7b14feb013ced887
Elena Duranceau <edurance@f1br6s18.clusters.42paris.fr>
2025-09-04 12:14:30 +0200

    echo w protections -nnnnnn

--- built-in/echo/ft_echo.c
+++ built-in/echo/ft_echo.c
@@ -6,23 +6,49 @@
 /*   By: edurance <edurance@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 10:50:42 by edurance          #+#    #+#             */
-/*   Updated: 2025/09/04 10:58:11 by edurance         ###   ########.fr       */
+/*   Updated: 2025/09/04 12:14:10 by edurance         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_built_in.h"
 
-int ft_echo(char **cmd, t_minishell *shell)
+static int	verify_flag(char *cmd)
+{
+	int	i;
+
+	i = 2;
+	while (cmd[i])
+	{
+		if (cmd[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static int	good_flag(int *i, char **cmd)
+{
+	int	flag;
+
+	flag = 0;
+	while (cmd[*i] && !ft_strncmp(cmd[*i], "-n", 2))
+	{
+		if (!verify_flag(cmd[*i]))
+			return (flag);
+		(*i)++;
+		flag++;
+	}
+	return (flag);
+}
+
+int	ft_echo(char **cmd, t_minishell *shell)
 {
 	int	i;
 	int	n_flag;
 
 	i = 1;
-	if (cmd[i] && !ft_strcmp(cmd[i], "-n"))
-	{
-		i++;
+	if (good_flag(&i, cmd))
 		n_flag = 1;
-	}
 	else
 		n_flag = 0;
 	while (cmd[i])
